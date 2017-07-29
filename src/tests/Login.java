63c6240914622f5b7b5d2f1f9002605cf3453fe4
Ann <annorl.em@gmail.com>
2017-07-29 16:04:53 +0300

    jeuue

--- src/tests/Login.java
+++ src/tests/Login.java
@@ -6,11 +6,11 @@ import org.testng.annotations.Test;
 public class Login extends MainClasses{
     @Test(testName = "Login",description = "login form", priority = 1)
     public void login() {
-        MainDriver.myWait(DRIVER, 10, ExpectedConditions.visibilityOfElementLocated(By.name("sUsername")), true);
-        DRIVER.findElement(By.name("sUsername1")).sendKeys("admin");
+        MainDriver.myWait(DRIVER, 10, ExpectedConditions.visibilityOfElementLocated(By.name("sUsername1")), true);
+        DRIVER.findElement(By.name("sUsername")).sendKeys("admin");
         DRIVER.findElement(By.name("password")).sendKeys("SKFs78AKLDnoa-");
         DRIVER.findElement(By.name("password")).submit();
-        boolean wait = MainDriver.myWait(DRIVER, 3, ExpectedConditions.visibilityOfElementLocated(
+        boolean wait = MainDriver.myWait(DRIVER, 5, ExpectedConditions.visibilityOfElementLocated(
                 By.cssSelector(".ui.warning.attached.message")), false);
         if (wait) {
             MainDriver.fail("Error login/password");
